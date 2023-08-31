32447f27799b90ff701859783bc065e0348f939b
Yassine Rakibi <yassine@arcraven.com>
2023-08-31 13:41:54 +0200

    task 10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - func to find depth of binary tree
+ * @tree: tree to be checked
+ * Return: 0 if tree is null
+*/
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (!tree || !tree->parent)
+		return (0);
+	return (1 + binary_tree_depth(tree->parent));
+}
