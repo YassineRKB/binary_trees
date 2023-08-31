d2fa2525bfe509b0e9358fcf32a6f9669ed0e8c1
Yassine Rakibi <yassine@arcraven.com>
2023-08-31 14:52:17 +0200

    task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,18 +8,16 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t h_left = 0, h_right = 0, r;
 
-	if (tree)
-	{
-		if (tree->left)
-			h_left = 1 + binary_tree_height(tree->left);
-		if (tree->right)
-			h_right = 1 + binary_tree_height(tree->right);
-		r = h_right;
-		if (h_left > h_right)
-			r = h_left;
-		return (r);
-	}
-	return (0);
+	if (!tree)
+		return (0);
+	if (tree->left)
+		h_left += 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		h_right += 1 + binary_tree_height(tree->right);
+	r = h_right;
+	if (h_left > h_right)
+		r = h_left;
+	return (r);
 }
 /**
  * binary_tree_balance - func to measure the balance factor of
